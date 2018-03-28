7a708af75b2ab021a157c1a7fdd4c5886522401c
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-03-28 02:44:20 -0500

    Link libsodium dynamically on unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,6 @@
 if [[ ${HOST} =~ .*linux.* ]] && [[ ${ARCH} == 32 ]]; then
     export CFLAGS="$CFLAGS -Og"
 fi
+export CPPFLAGS="$CPPFLAFS -I${PREFIX}/include"
+export SODIUM_INSTALL="system"
 python setup.py install --single-version-externally-managed --record record.txt
